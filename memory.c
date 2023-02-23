d12d7bc81763afec5bf2281862ed1fec0a5f9614
sunday0prime <sundayprime@gmail.com>
2023-02-22 18:00:14 -0800

    Simple Shell ALX project

---
+++ memory.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * bfree - frees a pointer and Nulls the address
+ * @pointer: address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0
+ */
+
+int bfree(void **pointer)
+{
+	if (pointer && *pointer)
+	{
+		free(*pointer);
+		*pointer = NULL;
+		return (1);
+	}
+
+	return (0);
+}
